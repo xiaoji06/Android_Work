60b60dafe29c6c1bf776ff0d971c580aad166007
xiaoji06 <171778415@qq.com>
2020-01-03 12:08:16 +0800

    第一次提交

---
+++ app/src/main/java/com/xiaoji/android_work/androidstudio/Setting.java
@@ -0,0 +1,26 @@
+package com.xiaoji.android_work.androidstudio;
+
+public class Setting {
+
+    /**
+     * 代码提示大小写忽略 设置
+     * File --> Settings -->Editor--> General ---> Code Completion -->Match case(不打勾)
+     */
+
+    /**
+     * 快捷键eclipse化
+     * File --> Settings -->Keymap --->Eclipse
+     */
+
+    /**
+     * 软件界面字体大小
+     * File --> Settings -->Appearance & Behavior --->Appearance -->Use custom font
+     */
+
+    /**
+     * 修改编辑区域字体
+     *  File --> Settings -->Editor-->Font
+     */
+
+
+}
